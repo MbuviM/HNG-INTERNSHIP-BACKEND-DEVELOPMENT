df6869339d79354191c7d172b5f8c63f6a61bde2
MbuviM <mwendembuvi10@gmail.com>
2023-09-10 11:32:20 +0300

    Final py file

--- task0xAPI_Endpoint_Creation/API.py
+++ task0xAPI_Endpoint_Creation/API.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 api_app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def info_api():
 
     # Check if required parameters are missing
     if not slack_name or not track:
-        return jsonify({"status": 400, "message": "Missing required parameters"}), 400
+        return '{"status": 400, "message": "Missing required parameters"}', 400
 
     # Get current day and time in UTC
     current_day = datetime.datetime.utcnow().strftime('%A')
@@ -23,32 +23,21 @@ def info_api():
     time_difference = current_time - request_time
 
     if abs(time_difference.total_seconds()) > 120:
-        return jsonify({"status": 400, "message": "Invalid UTC time"}), 400
-    
-    # Manually construct the response with the desired key order
-    response_data = {
-        "slack_name": slack_name,
-        "current_day": current_day,
-        "utc_time": utc_time,
-        "track": track,
-        "github_file_url": "https://github.com/MbuviM/HNG-Internship-Backend-Development/blob/main/task0xAPI_Endpoint_Creation/API.py",
-        "github_repo_url": "https://github.com/MbuviM/HNG-Internship-Backend-Development",
-        "status_code": 200
-    }
+        return '{"status": 400, "message": "Invalid UTC time"}', 400
 
-    # Arrange the dictionary in the desired order
-    response_data = {
-        "slack_name": response_data["slack_name"],
-        "current_day": response_data["current_day"],
-        "utc_time": response_data["utc_time"],
-        "track": response_data["track"],
-        "github_file_url": response_data["github_file_url"],
-        "github_repo_url": response_data["github_repo_url"],
-        "status_code": response_data["status_code"]
-    }
+    # Construct the JSON response as a string
+    response_data = (
+        '{"slack_name": "%s", '
+        '"current_day": "%s", '
+        '"utc_time": "%s", '
+        '"track": "%s", '
+        '"github_file_url": "https://github.com/MbuviM/HNG-Internship-Backend-Development/blob/main/task0xAPI_Endpoint_Creation/API.py", '
+        '"github_repo_url": "https://github.com/MbuviM/HNG-Internship-Backend-Development", '
+        '"status_code": 200}'
+    ) % (slack_name, current_day, utc_time, track)
 
-    # Returns the response in JSON format
-    return jsonify(response_data)
+    return response_data, 200, {'Content-Type': 'application/json'}
 
 if __name__ == '__main__':
     api_app.run(host='0.0.0.0', port=5000, debug=True)
+
