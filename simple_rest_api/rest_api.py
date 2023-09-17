f718af1b009963f1c6da96d642070b72fa5d8b76
MbuviM <mwendembuvi10@gmail.com>
2023-09-17 12:18:33 +0300

    Resolved Conflicts

--- simple_rest_api/rest_api.py
+++ simple_rest_api/rest_api.py
@@ -1,20 +1,19 @@
 from flask import Flask, request, jsonify
 from supabase import create_client
-from dotenv import load_dotenv
-import os
-#loads .env file 
-load_dotenv()
 
 # Initialize Flask application
 app = Flask(__name__)
 
 # Configure the Supabase database connection
-# supabase_url = "https://cicqhphudmrtycypnqnw.supabase.co"
-# supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpY3FocGh1ZG1ydHljeXBucW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ4MzQyNjMsImV4cCI6MjAxMDQxMDI2M30.rbvtSz4nxT7V-DPo718TB3G9EnIm7zZibRrOcpq-Pi8"
-supabase_url = os.environ.get('SUPABASE_URL')
-supabase_key = os.environ.get('SUPABASE_KEY')
+supabase_url = "https://cicqhphudmrtycypnqnw.supabase.co"
+supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpY3FocGh1ZG1ydHljeXBucW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ4MzQyNjMsImV4cCI6MjAxMDQxMDI2M30.rbvtSz4nxT7V-DPo718TB3G9EnIm7zZibRrOcpq-Pi8"
 supabase = create_client(supabase_url, supabase_key)
 
+# Define the Person model (optional, you can use Supabase tables directly)
+class Person:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
 # CREATE: Adding a new person
 @app.route('/api', methods=['POST'])
@@ -26,24 +25,22 @@ def add_person():
     if not isinstance(name, str):
         return jsonify({'error': 'Name must be a string.'}), 400
 
-    response = supabase.table('rest_api').insert({'name': name}).execute()
+    response = supabase.table('your_table_name').insert([{'name': name}])
     
-    if response.data:
-        return {'message': 'Person added successfully.'}, 201
+    if response['status_code'] == 201:
+        return jsonify({'message': 'Person added successfully.'}), 201
     else:
         return jsonify({'error': 'Failed to add person.'}), 500
-   
-
-    
 
 # READ: Fetching details
 @app.route('/api/<string:name>', methods=['GET'])
 def get_person_by_name(name):
-    response = supabase.table('rest_api').select('*').eq('name', name).execute()
-    person_data = response.data
+    response = supabase.table('your_table_name').select().eq('name', name)
+    person_data = response['data']
 
     if person_data:
-        return jsonify(person_data), 200
+        person = Person(person_data[0]['id'], person_data[0]['name'])
+        return jsonify({'id': person.id, 'name': person.name}), 200
     else:
         return jsonify({'error': 'Person not found.'}), 404
 
@@ -52,23 +49,19 @@ def get_person_by_name(name):
 def update_person_by_name(name):
     data = request.get_json()
     new_name = data.get('name')
-    #check if the name is in the table
-    response = supabase.table('rest_api').select('name').eq('name',name).execute()
-    person_data = response.data
-   
+
+    response = supabase.table('your_table_name').select().eq('name', name)
+    person_data = response['data']
+
     if not person_data:
         return jsonify({'error': 'Person not found.'}), 404
     
     person = person_data[0]
     person['name'] = new_name
 
-    # response = supabase.table('rest_api').upsert([person])
-    #update only if the name is found
-    #update ->set {new_name} in rest_api where name column is equal to the {name} passed 
-    response = supabase.table('rest_api').update({'name':new_name}).eq('name',name).execute()
-    to_updated_person_data = response.data
-
-    if  to_updated_person_data:
+    response = supabase.table('your_table_name').upsert([person])
+    
+    if response['status_code'] == 200:
         return jsonify({'message': 'Person updated successfully.'}), 200
     else:
         return jsonify({'error': 'Failed to update person.'}), 500
@@ -76,12 +69,12 @@ def update_person_by_name(name):
 # DELETE: Remove a person
 @app.route('/api/<string:name>', methods=['DELETE'])
 def delete_person_by_name(name):
-    response = supabase.table('rest_api').delete().eq('name', name).execute()
+    response = supabase.table('rest_api').delete().eq('name', name)
     
-    if response.data:
+    if response['status_code'] == 200:
         return jsonify({'message': 'Person deleted successfully.'}), 200
     else:
         return jsonify({'error': 'Failed to delete person.'}), 500
-    
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
