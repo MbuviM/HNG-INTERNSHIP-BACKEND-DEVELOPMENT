27c04d4dcd909d848e0f4fe2d1ed3dbf312501b1
MbuviM <mwendembuvi10@gmail.com>
2023-09-17 12:02:25 +0300

    API tests

--- simple_rest_api/tests.py
+++ simple_rest_api/tests.py
@@ -1,29 +1,48 @@
 import requests
 
-# Base URL of your API
-base_url = 'http://localhost:5000/api'
+## Base URL of your API
+base_url = 'https://cicqhphudmrtycypnqnw.supabase.co/rest/v1/rest_api'
+
+# Add your Supabase API key here
+api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpY3FocGh1ZG1ydHljeXBucW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ4MzQyNjMsImV4cCI6MjAxMDQxMDI2M30.rbvtSz4nxT7V-DPo718TB3G9EnIm7zZibRrOcpq-Pi8'
+
+# Define headers with the API key
+headers = {
+    'Content-Type': 'application/json',
+    'apikey': api_key,
+}
 
 # Test CREATE operation
 def test_create():
     response = requests.post(base_url, json={'name': 'Yvonne Mwende'})
-    print(response.json())
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
 
 # Test READ operation
 def test_read():
-    response = requests.get(f'{base_url}/Yvonne%20Mwende')
-    print(response.json())
-
+    response = requests.get(f'{base_url}?name=eq.Yvonne Mwende')
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
 
 # Test UPDATE operation
 def test_update():
-    response = requests.put(f'{base_url}/Yvonne%20Mwende', json={'name': 'Updated Name'})
-    print(response.json())
-
+    response = requests.patch(base_url, json=[{'name': 'Updated Name'}])
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
 
 # Test DELETE operation
 def test_delete():
-    response = requests.delete(f'{base_url}/Yvonne%20Mwende')
-    print(response.json())
+    response = requests.delete(f'{base_url}?name=eq.Yvonne Mwende')
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Error: {response.status_code}")
 
 # Run the tests
 test_create()
